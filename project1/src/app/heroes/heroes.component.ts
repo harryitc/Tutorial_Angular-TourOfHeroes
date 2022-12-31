c8037e887003110f45462382cd5126d44e023002
harryitc <cuongharryit@gmail.com>
2022-12-31 07:53:54 +0000

    Tour Of Heroes - Display A List

--- project1/src/app/heroes/heroes.component.ts
+++ project1/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,8 +11,14 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   }
+
+  heroes = HEROES
   constructor() { }
   ngOnInit(): void {
   }
 
+  selectedHero?: Hero
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }
---
+++ project1/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Phan Ngọc Cường' },
+    { id: 13, name: 'Nguyễn Thị Bích' },
+    { id: 14, name: 'Phan Ngọc Tú' },
+    { id: 15, name: 'Phan Tâm' },
+    { id: 16, name: 'Phan Thị Thanh Hiếu' },
+    { id: 17, name: 'Nguyễn Văn Năm' },
+    { id: 18, name: 'Nguyễn Thị Ngọc Diễn' },
+    { id: 19, name: 'Nguyễn Văn Ngọc' },
+    { id: 20, name: 'Nguyễn Thị Bích Lan' },
+]
