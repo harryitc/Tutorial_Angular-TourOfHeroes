606aa407ea3e7e4180a383ef14ff5bc3df0c94e6
harryitc <cuongharryit@gmail.com>
2022-12-31 11:07:15 +0000

    Tour Of Heroes - Add services - Add messages to hero service - Final

--- project1/src/app/messages/messages.component.ts
+++ project1/src/app/messages/messages.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
   constructor(public messageService: MessageService) {
+
+  }
+  ngOnInit(): void {
 
   }
 }
